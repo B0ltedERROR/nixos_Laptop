f4a98f75edc86c6609e01783ce4a9f952b154afa
B0ltedERROR <119495799+B0ltedERROR@users.noreply.github.com>
2025-05-09 09:53:46 -0400

    cleaned up dead files and packages
    removed lanzaboote and sbctl for now. Will likely do security updates in
    flakes when I get around to it

--- modules/default.nix
+++ modules/default.nix
@@ -6,15 +6,14 @@
 		./boot.nix
 		./fonts.nix
 		./desktop.nix
-		# ./hyprland.nix
-		# ./awesome.nix
-		# ./misc.nix
 		./networking.nix
 		./packages.nix
 		./programs.nix
 		./services.nix
-		# ./theme.nix
 		./users.nix
+
+# WIP
+#   ./awesome.nix
 	];
 
 }
